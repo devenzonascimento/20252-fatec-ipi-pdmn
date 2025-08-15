b722b0de15e6c3a3b52e2368e75a6f85e42002cb
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2025-08-15 19:53:20 -0300

    test(ap01-git): test subtraction function

--- intro-git/calculator-test.py
+++ intro-git/calculator-test.py
@@ -1,9 +1,10 @@
 import calculator
 
 x = 2
-
 y = 3
 
 sum_result = calculator.sum(x, y)
-
 print(f"{x} + {y} = {sum_result}")
+
+sub_result = calculator.sub(x, y)
+print(f"{x} + {y} = {sub_result}")
