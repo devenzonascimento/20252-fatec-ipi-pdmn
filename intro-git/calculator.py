bc0c4f828beb50ee6d68518e7f49c0d65545f4a6
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2025-08-15 20:01:52 -0300

    fix(ap01-git): fix subtraction function parameter

--- intro-git/calculator.py
+++ intro-git/calculator.py
@@ -2,7 +2,7 @@ def sum(op1: float, op2: float) -> float:
   return op1 + op2
 
 def sub(op1: float, op2: float) -> float:
-  return op1 - op1
+  return op1 - op2
 
 def mul(op1: float, op2: float) -> float:
   return op1 * op2
